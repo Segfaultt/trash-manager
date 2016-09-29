7088ee7a43805fc379e8e0d982036e2c77a30445
ShrimpKeeper <xophnex@gmail.com>
2016-09-29 22:03:01 +0100

    added the main code file included needed libraries added argument checking added a function to log new files

---
+++ trash-manager.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 			//outputting via CL
+#include <fstream> 				//interacting with log files
+#include <time.h> 				//logging time placed and checking current time
+#include <boost/filesystem.hpp>	//interacting with directories 
+
+bool log(std::string, std::fstream&, int&);
+
+int main(int argc, const char *argv[])
+{
+	//check the number of arguments 
+	if (argc < 2) {
+		std::cout<<"USAGE: trash-manager <trash-directory>\n";
+		return -1;
+	}
+
+	return 0;
+}
+
+//add an entry to the log
+bool log(std::string file, std::fstream& logFile, int& entries)
+{
+	logFile<<file<<' '<<time(NULL)<<'\n';
+
+	//return true if unsuccessful
+	if (logFile.fail()) {
+		//output error message
+		if (!logFile.is_open) {
+			std::cout<<"error: log file not open\n";
+		}
+		else if (logFile.eof()) {
+			std::cout<<"error: log file reached EOF\n";
+		}
+		else {
+				std::cout<<"error: writing to log file failed\n";
+		}
+		return true;
+	}
+	//if there are no errors return false
+	return false;
+}
