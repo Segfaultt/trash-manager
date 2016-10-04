e68c3bcc7f01fed872c3fc3cb9c2cc80b010346b
ShrimpKeeper <xophnex@gmail.com>
2016-10-04 12:58:39 +0100

    updated TODO.txt added some banner-like comments added newLogFile boolean to speed up checking if an entry needs to be added created a for loop to log and check the age of entries implemented entry logging

--- trash-manager.cpp
+++ trash-manager.cpp
@@ -3,7 +3,9 @@
 #include <time.h>//logging time placed and checking current time
 #include <boost/filesystem.hpp>//interacting with directories 
 
-bool log(std::string, std::fstream&, int&);
+bool log(std::string, std::fstream&);
+
+bool isLogged(std::string, std::fstream&);
 
 int main(int argc, const char *argv[])
 {
@@ -13,6 +15,9 @@ int main(int argc, const char *argv[])
 		return -1;
 	}
 
+	///////////////////////////////////////////////
+	//              Opening logfile              //
+	///////////////////////////////////////////////
 	//declare trashcan directory
 	boost::filesystem::path trash(argv[1]);
 
@@ -27,9 +32,9 @@ int main(int argc, const char *argv[])
 		std::cout<<"fatal error: specified trashcan does not exist\n";
 		return -1;
 	}
-
-	//open logfile in trashcan
+	//opening logfile
 	#define LOGFILE (trash.string()+".trash-log")
+	bool newLogfile = false;
 	std::fstream logfile;
 	logfile.open(LOGFILE);
 	//if there is no logfile then create one
@@ -41,6 +46,8 @@ int main(int argc, const char *argv[])
 		logfile.open(LOGFILE, std::ios_base::out | std::ios_base::trunc);
 		//open it with the needed flags 
 		logfile.open(LOGFILE, std::ios_base::out | std::ios_base::out | std::ios_base::trunc);
+		//set the newLogfile variable to true
+		newLogfile = true;
 		//check if it created a logfile
 		if (!logfile.is_open()) {
 			std::cout<<"fatal error: could not open logfile at "<<LOGFILE<<'\n';
@@ -54,11 +61,21 @@ int main(int argc, const char *argv[])
 		std::cout<<"successfully opened the existing logfile at "<<LOGFILE<<'\n';;
 	}
 
+	//for each file in the trashcan
+	for (boost::filesystem::directory_entry& entry : boost::filesystem::directory_iterator(trash)) {
+		//if it's an existing log file and the file is already logged 
+		if (!newLogfile && isLogged(entry.path().string(), logfile)) {
+		}
+		else {
+			log (entry.path().string(), logfile);
+		}
+	}
+
 	return 0;
 }
 
 //add an entry to the log
-bool log(std::string file, std::fstream& logFile, int& entries)
+bool log(std::string file, std::fstream& logFile)
 {
 	logFile<<file<<' '<<time(NULL)<<'\n';
 
@@ -77,5 +94,9 @@ bool log(std::string file, std::fstream& logFile, int& entries)
 		return true;
 	}
 	//if there are no errors return false
+	return false;
+}
+
+bool isLogged(std::string entry, std::fstream& logfile) {
 	return false;
 }
