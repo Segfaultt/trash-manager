98456a33d2a31627e1c2afe07d59f7d65dccda71
ShrimpKeeper <xophnex@gmail.com>
2016-10-31 23:24:02 +0800

    added -v flag for verbose and made quiet default

--- trash-manager.cpp
+++ trash-manager.cpp
@@ -3,22 +3,24 @@
 #include <time.h>//logging time placed and checking current time
 #include <boost/filesystem.hpp>//interacting with directories 
 
-bool log(std::string, std::fstream&);
+bool log(std::string, std::fstream&, bool);
 
 bool isLogged(std::string, std::fstream&);
 
 int main(int argc, const char *argv[])
 {
 	if (argc < 2) {//check the number of arguments 
-		std::cout<<"USAGE: trash-manager <trash-directory>\n";
+		std::cout<<"USAGE: trash-manager [-v] <trash-directory>\n";
 		return -1;
 	}
+
+	bool verbose = (argc > 2 && strncmp(argv[1], "-v", 2) == 0);//check for verbosity flag
 
 	///////////////////////////////////////////////
 	//              Opening logfile              //
 	///////////////////////////////////////////////
 
-	boost::filesystem::path trash(argv[1]);//open trashcan 
+	boost::filesystem::path trash(argv[argc-1]);//open trashcan using last argument
 
 	if (exists(trash)) {//check if trashcan exists
 		if (!is_directory(trash)) {//and is also a directory
@@ -36,6 +38,7 @@ int main(int argc, const char *argv[])
 	logfile.open(LOGFILE, std::ios_base::in | std::ios_base::out | std::ios_base::app);
 
 	if (!logfile.is_open()) {//if there is no logfile then create one
+		if (verbose)
 		std::cout<<"logfile does not exist, creating a new logfile\n";
 		logfile.clear();//clear iostate flags from original opening attempt 
 		logfile.open(LOGFILE, std::ios_base::out);//create a new file (no need for input)
@@ -44,13 +47,12 @@ int main(int argc, const char *argv[])
 			std::cout<<"fatal error: could not open logfile at "<<LOGFILE<<'\n';
 			return -1;
 		}
-		else {
-			std::cout<<"successfully created a logfile at "<<LOGFILE<<'\n';;
-		}
+		else if (verbose)
+		std::cout<<"successfully created a logfile at "<<LOGFILE<<'\n';;
 	}
-	else {
-		std::cout<<"successfully opened the existing logfile at "<<LOGFILE<<'\n';;
-	}
+	else if (verbose)
+	std::cout<<"successfully opened the existing logfile at "<<LOGFILE<<'\n';;
+	
 
 	///////////////////////////////////////////////////
 	//            Logging/deleting files             //
@@ -60,18 +62,22 @@ int main(int argc, const char *argv[])
 	for (boost::filesystem::directory_entry& entry : boost::filesystem::directory_iterator(trash)) {//for each file in the trashcan
 		oldPosition = logfile.tellg();
 		if (!newLogfile && isLogged(entry.path().string(), logfile)) {//if it's an existing log file and the file is already logged 
-			std::cout<<"entry \""<<entry.path().string()<<"\" is already logged\n";
 			logfile>>age;
-			std::cout<<"\tdate logged: "<<age<<std::endl;
+			if (verbose) {
+				std::cout<<"entry \""<<entry.path().string()<<"\" is already logged\n";
+				std::cout<<"\tdate logged: "<<age<<std::endl;
+			}
 			if (time(NULL) - age > 1210000) {//if it was logged more than 2 weeks ago
+				if (verbose)
 				std::cout<<'\t'<<entry.path().string()<<" is was logged more than 2 weeks ago. This file will be deleted\n";
 				remove(entry);//delete file
 			}
 		}
 		else if (entry.path().filename().string() != ".trash-log" && entry.path().filename().string() != ".DS_Store" ) {
 			logfile.seekg(oldPosition);//return to the position from before the search
-			log (entry.path().string(), logfile);
+			log (entry.path().string(), logfile, verbose);
 		}
+		if (verbose)
 		std::cout<<std::endl;
 	}
 
@@ -112,16 +118,21 @@ int main(int argc, const char *argv[])
 }
 
 //add an entry to the log
-bool log(std::string file, std::fstream& logfile)
+bool log(std::string file, std::fstream& logfile, bool verbose)
 {
-	if (logfile<<file<<'\n'<<time(NULL)<<'\n') { //if it successfully wrote to the file
-		std::cout<<"successfully logged entry \""<<file<<"\"\n";
-		return true;
-	}
-	else {
-		std::cout<<"failed logging entry \""<<file<<"\"\n";
-		return false;
-	}
+	bool successful;
+
+	if (logfile<<file<<'\n'<<time(NULL)<<'\n')//if it successfully wrote to the file
+	successful = true;
+	else 
+	successful = false;
+
+	if (!successful) 
+	std::cout<<"failed logging entry \""<<file<<"\"\n";
+	else if (verbose) 
+	std::cout<<"successfully logged entry \""<<file<<"\"\n";
+		
+	return successful;
 }
 
 //check if an entry is logged
